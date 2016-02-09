789ac9032dcd729ab1c165accb07c2b232237d7c
Mikolaj Wielocha <mwielocha@Mikolajs-Mac-Pro.local>
2016-02-09 22:07:11 +0100

    remove ???

--- src/main/scala/Scala99.scala
+++ src/main/scala/Scala99.scala
@@ -1,23 +1,23 @@
 object Scala99 {
 
-	def last(in: List[Int]): Option[Int] = ???
+	def last(in: List[Int]): Option[Int] = None
 
-	def penultimate(in: List[Int]): Option[Int] = ???
+	def penultimate(in: List[Int]): Option[Int] = None
 
-	def kth(k: Int, in: List[Int]): Option[Int] = ???
+	def kth(k: Int, in: List[Int]): Option[Int] = None
 
-	def length(in: List[Int]): Int = ???
+	def length(in: List[Int]): Int = 0
 
-	def reverse(in: List[Int]): List[Int] = ???
+	def reverse(in: List[Int]): List[Int] = Nil
 
-	def isPalindrome(in: List[Int]): Boolean = ???
+	def isPalindrome(in: List[Int]): Boolean = false
 
-	def flatten(in: List[Any]): List[Int] = ???
+	def flatten(in: List[Any]): List[Int] = Nil
 
-	def compress(in: List[Symbol]): List[Symbol] = ???
+	def compress(in: List[Symbol]): List[Symbol] = Nil
 
-	def pack(in: List[Symbol]): List[Symbol] = ???
+	def pack(in: List[Symbol]): List[Symbol] = Nil
 
-	def encode(in: List[Symbol]): List[Symbol] = ???
+	def encode(in: List[Symbol]): List[Symbol] = Nil
 
 }
